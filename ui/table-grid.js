5ee8f85a9d6cabf1ab022273f29909cf6bd106a0
dragon <rongjihuang@gmail.com>
2014-07-16 23:45:17 +0800

    word-wrap

--- ui/table-grid.js
+++ ui/table-grid.js
@@ -2,34 +2,37 @@
 function TableGridCtrl($scope) {
 	$scope.unit = "px";
 	var columnSize = 40;
-	var rowSize = 15;
+	var rowSize = 100;
 
-	$scope.columns = [];
+	var columns = [];
 	var w = 0;
 	var c;
 	for (var i = 1; i <= columnSize; i++) {
 		c = {
 			id: "C" + i, 
 			width: i == 1 ? 40 : 100, 
-			name: "列" + i
+			name: ((i % 5 == 0 && i > 1) ? "ccccccccccccccccccc" : "列") + i
 		};
-		$scope.columns.push(c);
+		columns.push(c);
 		w += c.width;
 	};
-	$scope.totalWidth = w + 2;
 
-	$scope.rows = [];
+	var rows = [];
 	var r;
 	for (var i = 1; i <= rowSize; i++) {
 		r = {};
 		for (var j = 1; j <= columnSize; j++) {
-			r["C" + j] = i + "-" + j;
+			r["C" + j] = "000" + i + "-" + j;
 			if (i % 5 == 0 && j > 1) {
 				r["C" + j] += " 长内容 长内容 长内容 长内容";
 			};
 		};
-		$scope.rows.push(r);
+		rows.push(r);
 	};
+
+	$scope.totalWidth = w + 2;
+	$scope.columns = columns;
+	$scope.rows = rows;
 }
 
 var $gb = angular.element(".body");
